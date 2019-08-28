6ace3dafec042fc7277f713fe574f06fd0f31468
Quentin Castel <quentin.castel@forgerock.com>
2019-08-28 11:19:38 +0100

    Missing -B

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@
 # This script will do a release of the artifact according to http://maven.apache.org/maven-release/maven-release-plugin/
 #git config --global user.email "codefresh@forgerock.com";
 #git config --global user.name "CodeFresh";
-mvn -s settings.xml -Dusername=$GITHUB_ACCESS_TOKEN release:prepare
-mvn -s settings.xml release:perform
+mvn -s settings.xml -Dusername=$GITHUB_ACCESS_TOKEN release:prepare -B
+mvn -s settings.xml release:perform -B
